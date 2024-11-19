2a6d6340008b18ef3c73a00f560216ae69b16471
sh1zoterika <hatekillhate@mail.ru>
2024-11-19 22:22:43 +0300

    Delete 29102.py

--- 29102.py
+++
@@ -1,9 +0,0 @@
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='1')
-channel.basic_publish(exchange='',
-                      routing_key='1',
-                      body='qweewq')
-connection.close()
